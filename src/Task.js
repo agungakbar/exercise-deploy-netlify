250e2be93e0ad5667529e26c97a60e3bb0c73465
Muhammad Agung Akbar <agungakbar400@gmail.com>
2023-06-19 10:00:09 +0700

    Initial Commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Muhammad Agung Akbar" // TODO: replace this
+const studentId = "FE4214698" // TODO: replace this
+
+export { url, studentName, studentId }
